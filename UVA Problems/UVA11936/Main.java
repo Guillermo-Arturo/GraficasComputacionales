95c40f5f8e82c08a8dfadb8acaca0734bfa3ae8a
GuillermoArt <a01321776@itesm.mx>
2018-01-29 11:33:42 -0600

    Se corrigió el error de finalizar el problema en UVA 11936

--- UVA Problems/UVA11936/Main.java
+++ UVA Problems/UVA11936/Main.java
@@ -1,32 +1,29 @@
-import java.io.File;
 import java.util.Scanner;
-import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main{
 
     public static void main(String args[]){
         int numberOfData;
-        try{
-            File data = new File(args[0]);
-            Scanner scanner = new Scanner(data);
+        Scanner scanner = new Scanner(System.in);
+        
+        while(scanner.hasNext()){
+            numberOfData = scanner.nextInt();
 
-            numberOfData = Integer.parseInt(scanner.nextLine());
-            
-            while(scanner.hasNextLine()){
-                StringTokenizer tokens = new StringTokenizer(scanner.nextLine());
-                int a = Integer.parseInt(tokens.nextToken());
-                int b = Integer.parseInt(tokens.nextToken());
-                int c = Integer.parseInt(tokens.nextToken());
+            while(numberOfData > 0){
+           
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                int c = scanner.nextInt();
 
                 if(a + b > c && a + c > b && b + c > a){
                     System.out.println("OK");
                 }else{
                     System.out.println("Wrong!!");
                 }
+                numberOfData --;
             }
-        }catch(IOException e){
-
         }
+
+        System.exit(0);
     }
 }
